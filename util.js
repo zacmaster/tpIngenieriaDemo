91049efef12916fced9cc1dc42ea584a66c81424
Zacarías Samaniego <zacariassamaniego@gmail.com>
2019-03-05 12:41:08 -0300

    Se agrega url de leaflet

--- util.js
+++ util.js
@@ -1,3 +1,31 @@
 const dqsa = args => document.querySelectorAll(args)
 const dqs = args => document.querySelector(args)
 const apiUrl = "https://infraccionesya.herokuapp.com/api/"
+
+
+/******************************************************************************
+ * Función para crear un mapa.
+ */
+function crearMapa(nodeId){
+    // Ubicación de la UNGS.
+    var ungsLocation = [-34.5221554, -58.7000067];
+
+    // Creación del componente mapa de Leaflet.
+    var map = L.map(nodeId).setView(ungsLocation, 13);
+
+    // Agregamos los Layers de OpenStreetMap.
+    var baseLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Agregamos el control para seleccionar Layers al mapa
+    var layersControl = L.control.layers({
+        "Base": baseLayer
+    });
+    layersControl.addTo(map);
+    // hack:
+    map.layersControl = layersControl;
+
+    return map;
+}
+
