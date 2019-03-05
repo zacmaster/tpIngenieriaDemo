add779a4faeab61343bb0a55cc98251e55002abd
JereMedr <jeremedrano228@gmail.com>
2019-03-05 10:14:31 -0300

    se muestran infracciones

--- util.js
+++ util.js
@@ -1,2 +1,3 @@
 const dqsa = args => document.querySelectorAll(args)
 const dqs = args => document.querySelector(args)
+const apiUrl = "https://infraccionesya.herokuapp.com/api/"
--- vista1.js
+++ vista1.js
@@ -3,5 +3,44 @@ const consultarInfraccion = (e) =>{
     e.preventDefault()
     console.log('Consultando patente...')
     const resultado = 'El resultado es '
-    dqs('#resultado').innerHTML = resultado + dqs('#input').value
+    //   dqs('#resultado').innerHTML = resultado + dqs('#input').value
+    let patente = dqs('#input').value
+    getInfracciones(patente)
+}
+
+function getInfracciones(patente){
+    fetch(apiUrl + patente + '/infracciones/')
+    .then(r => r.json())
+    .then(r => mostrarInfracciones(r.infracciones)) 
+}
+
+function mostrarInfracciones(infracciones){
+    var infraccionesHTML = `
+    
+    <table>
+        <thead>
+        <tr>
+            <th>Direcciones</th>
+            <th>Monto</th>
+            <th>Tipo</th>
+        </tr>
+        </thead>
+        <tbody>
+
+    `
+    
+    infracciones.forEach(e => {
+        infraccionesHTML += `
+        
+            <tr>
+                <td>${e.direccionRegistrada}</td>
+                <td>${e.montoAPagar}</td>
+                <td>${e.tipoInfraccion}</td>  
+            </tr>
+        `
+    });
+    infraccionesHTML += `</tbody></table>`
+    dqs('#infracciones').innerHTML = infraccionesHTML
+
+
 }
